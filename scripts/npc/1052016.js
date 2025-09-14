b1c1b28f157954524304b6bb5351f12d9ba0bc34
Erwin Evans <zew732@my.utsa.edu>
2025-09-14 11:06:34 -0700

    fix cab

--- scripts/npc/1052016.js
+++ scripts/npc/1052016.js
@@ -1,34 +1,26 @@
-var maps = [104000000, 102000000, 101000000, 100000000];
-var prices = [1000, 800, 1200, 1000];
+// Regular Cab
 
-npc.sendNext("Hi! I drive the Regular Cab. If you want to go from town to town safely and fast, then ride our cab. We'll gladly take you to your destination with an affordable price.");
+var towns = [104000000, 102000000, 101000000, 103000000]
+var prices_text = ["800", "1,000", "1,000", "1,200"]
+var prices_num = [800, 1000, 1000, 1200]
 
-var isBeginner = plr.job() == 0;
+npc.sendBackNext("How's it going? I drive the Regular Cab. If you want to go from town to town safely and fast, then ride our cab. We'll gladly take you to your destination with an affordable price", false, true)
 
-var text = "";
-if (isBeginner) {
-    text += "We have a special 90% discount for beginners. ";
+var text = "Choose your destination, for fees will change from place to place.\r\n"
+
+for (var i = 0; i  < towns.length; i++) {
+    text += "#L" + i + "##b#m" + towns[i] + "# (" + prices_text[i] +" mesos)#l \r\n"
 }
-text += "Choose your destination, for fees will change from place to place.#b";
-for (var i = 0; i < maps.length; i++) {
-    var price = isBeginner ? prices[i] / 10 : prices[i];
-    text += "\r\n#L" + i + "##m" + maps[i] + "# (" + price + " mesos)#l";
-}
-npc.sendSelection(text);
 
-var sel = npc.selection();
-if (sel < 0 || sel >= maps.length) sel = 0;
+npc.sendSelection(text)
 
-var map = maps[sel];
-var price = isBeginner ? prices[sel] / 10 : prices[sel];
-
-if (npc.sendYesNo("You don't have anything else to do here, huh? Do you really want to go to #b#m" + map + "##k? It'll cost you #b" + price + " mesos#k.")) {
-    if (plr.mesos() >= price) {
-        plr.giveMesos(-price);
-        plr.warp(map);
+if (npc.sendYesNo("You don't have anything else to do here, huh? Do you really want to go to #b#m" + towns[npc.selection()] + "# #k? It'll cost you #b" + prices_text[npc.selection()] + " mesos")) {
+    if (plr.mesos() < prices_num[npc.selection()]) {
+        npc.sendOk("You don't have enough mesos! Come back when you do.")
     } else {
-        npc.sendNext("You don't have enough mesos. Sorry to say this, but without them, you won't be able to ride the cab.");
+        plr.giveMesos(-1 * prices_num[npc.selection()])
+        plr.warp(towns[npc.selection()])
     }
 } else {
-    npc.sendNext("There's a lot to see in this town, too. Come back and find us when you need to go to a different town.");
+    npc.sendBackNext("There's a lot to see in this town, too. Come back and find us when you need to go to a different town.", false, true)
 }
